0bb66d2d6a5fe558c7fe67dee3f8a5a0e7a8f900
Whitehurst <jwhitehurst@ph.lcl>
2022-05-26 13:05:12 -0700

    Add APCD export/import directory with all of the functions, files and SSIS package to export the APCD data and then import it

---
+++ apcd_export_import/apcd_export_filename_fix.R
@@ -0,0 +1,18 @@
+library(stringr)
+library(R.utils)
+
+base_dir <- "//phcifs/SFTP_DATA/APDEDataExchange/WA-APCD/"
+export_dir <- paste0(base_dir,"export/")
+load_date <- "20220530"
+
+file_list <- list.files(path = export_dir,
+                        recursive = T,
+                        full.names = T)
+
+for(file in file_list) {
+  new_file <- str_replace_all(file, ".txt", "")
+  new_file <- str_replace(new_file, ".gz.0", ".0")
+  new_file <- str_replace(new_file, ".part", ".txt")
+  new_file <- str_replace(new_file, ".txt", paste0("_", load_date, ".txt"))
+  file.rename(file, new_file)
+}
